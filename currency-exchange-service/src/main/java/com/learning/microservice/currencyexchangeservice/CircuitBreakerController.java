914c96c8a626b7637a18298471f3c60d2c4ac219
thoufic shahul hameed <thouficshahulhameed@gmail.com>
2021-02-27 01:00:25 -0600

    Circuit breaker Resilience4j

--- currency-exchange-service/src/main/java/com/learning/microservice/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/learning/microservice/currencyexchangeservice/CircuitBreakerController.java
@@ -1,5 +1,30 @@
 package com.learning.microservice.currencyexchangeservice;
 
-public class CircuitBreakerController {
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+
+
+
+@RestController
+public class CircuitBreakerController {	
+	
+	@GetMapping("/pi")
+	//@Retry(name = "sample-api" , fallbackMethod= "hardcodedResponse")
+	@CircuitBreaker(name = "sample-api" , fallbackMethod= "hardcodedResponse")
+	public String sampleApi() {
+		System.out.println("Sample api received thrre times");
+		ResponseEntity<String> forEntity= new RestTemplate().getForEntity("http://localhost:8080/some-dummy", String.class);
+		
+		return forEntity.getBody();
+	}
+	
+	public String hardcodedResponse(Exception ex) {
+		return "Nothing to worry about";
+	}
 
 }
